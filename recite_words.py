72429c2f2a1bf14390a4c85be17e8c917b057ce9
FOTH0626 <1991808595@qq.com>
2023-03-14 09:53:59 +0800

    ...

--- recite_words.py
+++ recite_words.py
@@ -1,22 +1,36 @@
-""""选择背诵单词数目"""
+"""背单词系统需要函数：
+读取用户单词数（已完成
+随机生成单词:读取单词数，生成对应数量的随机数，寻找对应的单词，展示
+读取输入是否正确
+读取选择是否正确
+错误单词导入错题本
+错题本修改，删除
+等功能"""
+
+
 import random
 
 
+"""选择背诵单词数目"""
+
+
 def choose_amount():
-    amount=input("请选择你想背的单词数量,请选择好后回车确定")
+    amount = input("请选择你想背的单词数量,请选择好后回车确定")
     return amount
 
 
-"""英译中
-给出单词及对应中文含义"""
+"""用于生成随机数"""
+
+
+def random_words():
+    amount = choose_amount()
+    with open('words.csv', "r", encoding='utf-8') as f:
+        max_number = len(f.readlines())
+        random.sample(range(1, max_number), amount)
+
+
+"""英译中给出单词及对应中文含义"""
 
 
 def english_translate_chinese():
     choose_amount()
-
-
-
-
-
-
-
