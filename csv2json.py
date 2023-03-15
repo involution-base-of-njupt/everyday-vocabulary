f56b47b37141bb85ffbf280ce0010c59ae746e84
Glucy2 <Glucy2@126.com>
2023-03-15 14:19:41 +0800

    csv2json

---
+++ csv2json.py
@@ -0,0 +1,17 @@
+import csv
+import json
+
+def csv2json(csv_file, json_file):
+    with open(csv_file, 'r', newline='',encoding='utf-8') as f:
+        reader = csv.reader(f)
+        if reader == None:
+            return False
+        json_data = {}
+        for row in reader:
+            json_data[row[0]] = row[1]
+        with open(json_file, 'w', encoding='utf-8') as f:
+            json.dump(json_data, f, ensure_ascii=False, indent=4)
+        return True
+
+if __name__ == '__main__':
+    csv2json('words.csv', 'words.json')
