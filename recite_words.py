8f793a93924f7dab271028cca1547fb5a82b1188
Glucy2 <Glucy2@126.com>
2023-03-16 23:00:34 +0800

    寄

--- recite_words.py
+++ recite_words.py
@@ -15,7 +15,7 @@ import random
 import word_manage
 
 
-word_dict = word_manage.get_all()  # 所有单词的字典
+word_dict = word_manage.get_all()[1]  # 所有单词的字典
 amount_all = len(word_dict)  # 单词总数
 word_list = list(word_dict)  # 所有单词的列表，仅英文
 
@@ -23,30 +23,30 @@ word_list = list(word_dict)  # 所有单词的列表，仅英文
 # 选择背诵单词数目
 def choose_amount():
     while True:
-        amount = input("请选择你想背的单词数量，请选择好后回车确定：")
+        amount = input('请选择你想背的单词数量，请选择好后回车确定：')
         try:
             amount = int(amount)
             if amount > amount_all or amount <= 0:
-                print("单词总数为%d，请重新输入" % amount_all)
+                print('单词总数为%d，请重新输入' % amount_all)
                 continue
             else:
                 return amount
         except ValueError:
-            print("请输入整数数字")
+            print('请输入整数数字')
             continue
 
 
 # 进行选项数量选择
 def choice_choose():
     while True:
-        choice_number = input("请选择选项数量（数量在1到9之间），默认为4个，如果不想改变，请输入0退出选择")
+        choice_number = input('请选择选项数量（数量在1到9之间），默认为4个，如果不想改变，请输入0退出选择')
         try:
             choice_number = int(choice_number)
         except Exception as e:
             print('发生错误：', e, '请重新输入')
             continue
         if choice_number < 0:
-            print("请重新输入")
+            print('请重新输入')
             continue
         elif choice_number == 0:
             choice_number = 4
@@ -73,11 +73,11 @@ def english_translate_chinese():
     words_test = random.sample(word_list, amount)
     for i in words_test:
         answer_list = {}
-        for j in choice_number:
+        for j in range(choice_number):
             if j == 1:
                 answer_list[0] = word_dict[i]
             else:
-                answer_list[j] = random.sample(word_dict.values())
+                answer_list[j] = random.sample(word_dict.values(), choice_number)
         print('请选择单词的正确中文含义')
         print()
 
