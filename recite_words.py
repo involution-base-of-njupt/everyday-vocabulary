dbc50e3207014ceba002aaa2ba1ee49177a18607
Glucy2 <Glucy2@126.com>
2023-03-17 08:51:44 +0800

    英译中

--- recite_words.py
+++ recite_words.py
@@ -15,9 +15,15 @@ import random
 import word_manage
 
 
-word_dict = word_manage.get_all()[1]  # 所有单词的字典
-amount_all = len(word_dict)  # 单词总数
-word_list = list(word_dict)  # 所有单词的列表，仅英文
+def get_dict():
+    global word_dict, amount_all, word_list
+    word_dict = word_manage.get_all()[1]  # 所有单词的字典
+    amount_all = len(word_dict)  # 单词总数
+    word_list = list(word_dict)  # 所有单词的列表，仅英文
+    if word_dict == {}:
+        return False
+    else:
+        return True
 
 
 # 选择背诵单词数目
@@ -39,7 +45,7 @@ def choose_amount():
 # 进行选项数量选择
 def choice_choose():
     while True:
-        choice_number = input('请选择选项数量（数量在1到9之间），默认为4个，如果不想改变，请输入0退出选择')
+        choice_number = input('请选择选项数量（数量在2到9之间），建议为4个')
         try:
             choice_number = int(choice_number)
         except Exception as e:
@@ -48,10 +54,10 @@ def choice_choose():
         if choice_number < 0:
             print('请重新输入')
             continue
-        elif choice_number == 0:
-            choice_number = 4
-            break
-        elif 0 < choice_number < 10:
+        elif choice_number < 2:
+            print('请重新输入')
+            continue
+        elif 1 < choice_number < 10:
             break
         else:
             continue
@@ -67,19 +73,46 @@ def random_words():
 
 # 英译中
 def english_translate_chinese():
-    amount = choose_amount()
-    print('你选择了%d个单词' % amount)
-    choice_number = choice_choose()
-    words_test = random.sample(word_list, amount)
-    for i in words_test:
-        answer_list = {}
-        for j in range(choice_number):
-            if j == 1:
-                answer_list[0] = word_dict[i]
-            else:
-                answer_list[j] = random.sample(word_dict.values(), choice_number)
-        print('请选择单词的正确中文含义')
-        print()
+    if get_dict():
+        amount = choose_amount()
+        print('你选择了%d个单词' % amount)
+        choice_number = choice_choose()
+        words_test = random.sample(word_list, amount)
+        for en in words_test:
+            answer_list = {}
+            for ans in range(choice_number):
+                if not ans:
+                    answer_list[0] = word_dict[en]
+                else:
+                    answer_list[ans] = word_dict.popitem()[1]
+            print('请选择单词{}的正确中文含义：'.format(en))
+            # 设置答案选项
+            for ans in range(choice_number):
+                answer = answer_list.popitem()[1]
+                if answer == word_dict[en]:
+                    correct_answer = ans + 1
+                print('''
+    {}. {}'''.format(ans + 1, answer))
+            print('\n')
+            while True:
+                answer = input('请输入你的答案：')
+                try:
+                    answer = int(answer)
+                    if answer == correct_answer:
+                        print('回答正确')
+                        break
+                    elif answer > choice_number or answer <= 0:
+                        print('请重新输入')
+                        continue
+                    else:
+                        print('回答错误')
+                        break
+                except ValueError:
+                    print('请输入数字')
+                    continue
+    else: # 数据库为空
+        print('Dictionary is empty, please add words first.')
+        return
 
 
 if __name__ == '__main__':
