05a8ed99904b1e2088763e3a3270fd49faa0822d
Glucy2 <Glucy2@126.com>
2023-03-21 10:40:27 +0800

    修复导入错误

--- import_file.py
+++ import_file.py
@@ -18,23 +18,21 @@ def csv2json(csv_file, json_file, codec):
         json.dump(json_data, f, indent=4)
         return None
     except Exception as e:
-        print('Error when converting csv to json: ', e)
         return e
     finally:
         if f:
             f.close()
 
-# 导入CSV文件，返回值是发生的错误 TODO: bug修复
+# 导入CSV文件，返回值是发生的错误
 def csv_import(file, codec, mode):
     f = None
     try:
         f = open(file, 'r', newline='',encoding=codec)
         reader = csv.reader(f)
         for row in reader:
-            word_manage.add(row[0], row[1], mode)
+            word_manage.write(row[0], row[1], mode)
         return None
     except Exception as e:
-        print('Error when importing csv file: ', e)
         return e
     finally:
         if f:
@@ -46,10 +44,9 @@ def json_import(file, codec, overwrite):
         f = open(file, 'r', newline='',encoding=codec)
         word_dict = json.load(f)
         for en in word_dict:
-            word_manage.add(en, word_dict[en], overwrite)
+            word_manage.write(en, word_dict[en], overwrite)
         return None
     except Exception as e:
-        print('Error when importing json file: ', e)
         return e
     finally:
         f.close()
