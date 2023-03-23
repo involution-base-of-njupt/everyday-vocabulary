9bf0af3eca4fb6a264a5f4a1adf1dcf463e34054
ZieLinX <113131835+ZieLinX@users.noreply.github.com>
2023-03-23 08:59:42 +0800

    用户改自己窗口在用户主窗口的调用

--- gui/user1_ui.py
+++ gui/user1_ui.py
@@ -6,6 +6,7 @@ from gui import ec_ui
 from gui import ce_ui
 from gui import searchword_ui
 from gui import wordlist_ui
+from gui import user_change_password_ui
 
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
@@ -71,7 +72,8 @@ class user1(QWidget):
         pass
 
     def clickedchangepassword(self):
-        pass
+        self.user_change_password_window = user_change_password_ui.user_change_password()
+        self.user_change_password_window.ui.show()
 
     def clickedexit(self):
         self.account_window = account_ui.account()
